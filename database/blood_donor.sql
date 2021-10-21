e99dd1be199d26c477b6ba6f11379e1876149df8
quanhay3 <quanhay23@gmail.com>
2021-10-21 09:10:37 +0700

    database

---
+++ database/blood_donor.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th10 21, 2021 lúc 04:08 AM
+-- Phiên bản máy phục vụ: 10.4.21-MariaDB
+-- Phiên bản PHP: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `ktgk_61ht`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `blood_donor`
+--
+
+CREATE TABLE `blood_donor` (
+  `bd_id` int(11) NOT NULL,
+  `bd_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `bd_sex` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `bd_age` int(11) DEFAULT NULL,
+  `bd_bgroup` char(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `bd_reg_date` date DEFAULT NULL,
+  `bd_phno` varchar(13) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `blood_donor`
+--
+
+INSERT INTO `blood_donor` (`bd_id`, `bd_name`, `bd_sex`, `bd_age`, `bd_bgroup`, `bd_reg_date`, `bd_phno`) VALUES
+(1, 'Nguyễn Ngọc Lan', 'Nữ', 12312, 'B', '2021-10-15', '09822685456'),
+(2, 'Nguyễn Thị Thụ', 'Nữ', 1997, 'A', '2021-10-16', '0987952147'),
+(3, 'Nguyễn Ngọc Long', 'Nam', 2000, 'B', '2021-10-19', '0979500226');
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `blood_donor`
+--
+ALTER TABLE `blood_donor`
+  ADD PRIMARY KEY (`bd_id`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `blood_donor`
+--
+ALTER TABLE `blood_donor`
+  MODIFY `bd_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
